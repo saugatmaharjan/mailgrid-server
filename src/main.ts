5756abe3e4a94c53dad9dfb3e2e395001607ed89
saugatmaharjan <saugat@fusemachines.com>
2020-02-12 09:46:46 +0545

    cors changes

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://inspiring-kare-d8a73a.netlify.com',
+    methods: ['GET', 'PUT', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT || 3000);
 }
